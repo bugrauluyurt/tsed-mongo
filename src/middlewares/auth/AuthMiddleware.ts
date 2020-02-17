68fd20ada4698f74eb848d52fd8277e0ab5b736d
Bugra Uluyurt <bugra.u@gmail.com>
2020-02-16 20:10:05 -0500

    Prettified the code

--- src/middlewares/auth/AuthMiddleware.ts
+++ src/middlewares/auth/AuthMiddleware.ts
@@ -1,33 +1,49 @@
-import {
-    EndpointInfo,
-    Next,
-    Req,
-    Middleware,
-    IMiddleware
-} from "@tsed/common";
+import { EndpointInfo, IMiddleware, Middleware, Next, Req } from "@tsed/common";
+import * as _ from "lodash";
 import { User } from "src/models/users/User";
 import { Forbidden, Unauthorized } from "ts-httpexceptions";
 import { createCustomErrorBody } from "../../models/customErrors/CustomErrorBody";
 import { AuthMiddlewareErrorKeys, AuthMiddlewareErrorMessages } from "./errors/AuthMiddlewareErrors";
-import * as _ from "lodash";
 
 @Middleware()
 export class AuthMiddleware implements IMiddleware {
-    public use(@Req() request: Express.Request, @EndpointInfo() endpoint: EndpointInfo, @Next() next: Express.NextFunction) {
+    public use(
+        @Req() request: Express.Request,
+        @EndpointInfo() endpoint: EndpointInfo,
+        @Next() next: Express.NextFunction
+    ) {
         const options = endpoint.get(AuthMiddleware) || {};
 
-        if (!request.isAuthenticated()) { // passport.js method to check auth
-            const error = new Unauthorized(AuthMiddlewareErrorMessages[AuthMiddlewareErrorKeys.UNAUTHORIZED]);
-            error.body = createCustomErrorBody(AuthMiddlewareErrorKeys.UNAUTHORIZED, AuthMiddlewareErrorMessages);
+        if (!request.isAuthenticated()) {
+            // passport.js method to check auth
+            const error = new Unauthorized(
+                AuthMiddlewareErrorMessages[
+                    AuthMiddlewareErrorKeys.UNAUTHORIZED
+                ]
+            );
+            error.body = createCustomErrorBody(
+                AuthMiddlewareErrorKeys.UNAUTHORIZED,
+                AuthMiddlewareErrorMessages
+            );
             return next(error);
         }
 
-        const {roles: userRoles}: Partial<User> = request.user;
-        const isRoleAuthenticated = _.some(userRoles, (userRole) => _.includes(options.roles, userRole));
+        const { roles: userRoles }: Partial<User> = request.user;
+        const isRoleAuthenticated = _.some(userRoles, userRole =>
+            _.includes(options.roles, userRole)
+        );
 
         if (!isRoleAuthenticated) {
-            const forbiddenError = new Forbidden(AuthMiddlewareErrorMessages[AuthMiddlewareErrorKeys.FORBIDDEN]);
-            return next({...forbiddenError, body: createCustomErrorBody(AuthMiddlewareErrorKeys.FORBIDDEN, AuthMiddlewareErrorMessages)});
+            const forbiddenError = new Forbidden(
+                AuthMiddlewareErrorMessages[AuthMiddlewareErrorKeys.FORBIDDEN]
+            );
+            return next({
+                ...forbiddenError,
+                body: createCustomErrorBody(
+                    AuthMiddlewareErrorKeys.FORBIDDEN,
+                    AuthMiddlewareErrorMessages
+                )
+            });
         }
         next();
     }
--- src/middlewares/errorHandler/ErrorHandlerMiddleware.ts
+++ src/middlewares/errorHandler/ErrorHandlerMiddleware.ts
@@ -7,19 +7,18 @@ import CustomError from "../../models/customErrors/CustomError";
 @OverrideProvider(GlobalErrorHandlerMiddleware)
 export class ErrorHandlerMiddleware extends GlobalErrorHandlerMiddleware {
     use(@Err() error: any, @Req() request: Req, @Res() response: Res): any {
-        console.log("[Log]: ErrorHandlerMiddleware -> error", error);
         if (error instanceof Exception || error.status) {
             const customError = new CustomError({
                 status: _.get(error, "status", 404),
                 type: _.get(error, "type"),
                 key: _.get(error, "body.key"),
                 message: _.get(error, "body.message"),
-                origin: _.get(error, "origin") || _.get(request, "url"),
+                origin: _.get(error, "origin") || _.get(request, "url")
             });
             if (isDev()) {
-              request.log.error({
-                error: { customError, stack: _.get(error, "stack") },
-              });
+                request.log.error({
+                    error: { customError, stack: _.get(error, "stack") }
+                });
             }
             this.setHeaders(response, error, error.origin);
             response.status(error.status).json(customError);
