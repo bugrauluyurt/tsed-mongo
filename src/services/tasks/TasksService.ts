3274b24976f46e190164c9c047e78557504e17a9
Bugra Uluyurt <bugra.u@gmail.com>
2020-06-06 19:53:58 -0400

    Tasks services now includes add and update functionalities

--- src/services/tasks/TasksService.ts
+++ src/services/tasks/TasksService.ts
@@ -5,6 +5,7 @@ import { Task } from "src/models/tasks/Task";
 import { BadRequest } from "ts-httpexceptions";
 import { ERROR_TASK_ID_MISSING } from "../../errors/TasksError";
 import { ERROR_PROJECT_SECTION_ID_MISSING } from "../../errors/ProjectSectionsError";
+import * as _ from "lodash";
 
 @Service()
 export class TasksService {
@@ -31,5 +32,23 @@ export class TasksService {
     async addTask(task: Task): Promise<Task> {
         const model = new this.Task(task);
         return await this.Task.create(model);
+    }
+
+    async updateTask(taskId: string, taskPartial: Partial<Task>): Promise<Task> {
+        if (_.isEmpty(taskId)) {
+            throw new BadRequest(ERROR_TASK_ID_MISSING);
+        }
+        return await this.Task.findByIdAndUpdate(taskId, taskPartial, {
+            omitUndefined: true,
+            new: true,
+            runValidators: true,
+        }).exec();
+    }
+
+    async removeTask(taskId: string): Promise<Task> {
+        if (_.isEmpty(taskId)) {
+            throw new BadRequest(ERROR_TASK_ID_MISSING);
+        }
+        return await this.Task.findByIdAndDelete(taskId).exec();
     }
 }
